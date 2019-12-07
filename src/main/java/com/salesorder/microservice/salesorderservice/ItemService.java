d13985288c9389da98d57e61eaa481118f77da20
Shyam Gorai <spg.144@gmail.com>
2019-12-08 02:08:10 +0530

    Code modified

--- src/main/java/com/salesorder/microservice/salesorderservice/ItemService.java
+++ src/main/java/com/salesorder/microservice/salesorderservice/ItemService.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.salesorder.microservice.salesorderservice.bo.SalesOrder;
 
 
-@FeignClient(name ="item-service")
+@FeignClient(name ="zuul-edge-server")
 @RibbonClient(name ="item-service")
 public interface ItemService {
 	
-	@GetMapping("/items/{itemname}")
+	@GetMapping("/item-service/items/{itemname}")
 	public SalesOrder findItem(@PathVariable("itemname") String itemName);
 
 }
