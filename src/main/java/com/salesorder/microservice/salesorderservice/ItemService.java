09ff63d73057d57ebc5d6699ccefe4796735f53f
student <student@win2012base>
2019-12-08 08:52:44 -0800

    Zuul added

--- src/main/java/com/salesorder/microservice/salesorderservice/ItemService.java
+++ src/main/java/com/salesorder/microservice/salesorderservice/ItemService.java
@@ -5,19 +5,17 @@ import javax.ws.rs.Produces;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.salesorder.microservice.salesorderservice.bo.SalesOrder;
 
-@FeignClient(name ="item-service", url="localhost:8801")
-//@FeignClient(name ="zuul-edge-server")
-//@RibbonClient(name ="item-service")
+//@FeignClient(name ="item-service")
+@FeignClient(name ="zuul-edge-server")
+@RibbonClient(name ="item-service")
 public interface ItemService {
 	
-	@GetMapping("/item/{itemname}")
+	@GetMapping("/item-service/item/{itemname}")
 	public SalesOrder findItem(@PathVariable("itemname") String itemname);
 
 }
--- src/main/java/com/salesorder/microservice/salesorderservice/SalesOrderController.java
+++ src/main/java/com/salesorder/microservice/salesorderservice/SalesOrderController.java
@@ -8,8 +8,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
